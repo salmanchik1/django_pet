d9e6b19b0ff71a5cccda46067250df32c2b75ab7
salmanchik1 <salmanov.ramis@gmail.com>
2022-08-18 00:52:03 +0300

    migrated database from sqlite3 to postgresql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysite',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -87,11 +91,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
+    }, { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', }, {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
