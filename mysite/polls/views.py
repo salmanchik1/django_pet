5bff036426d935501a481f09f20602445bec2df4
salmanchik1 <salmanov.ramis@gmail.com>
2022-08-25 00:40:54 +0300

    modified index view in polls app. addex index template in polls app instead of simple string in response added rendered string.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,8 +5,11 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse(f"You're looking at question {question_id}.")
