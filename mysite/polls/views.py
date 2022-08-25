27cc0a62da24849800527aa00eb57dfe2b5043e7
salmanchik1 <salmanov.ramis@gmail.com>
2022-08-25 23:13:26 +0300

    set detail view of polls app to keep uncrashed on question not exist exception

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
 from .models import Question
@@ -12,7 +12,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist.")
+    return render(request, 'polls/detail.html', {'question:': question})
 
 def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}.")
