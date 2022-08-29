83227c72f929486bee0b27a41619788f9f55bfaa
salmanchik1 <salmanov.ramis@gmail.com>
2022-08-29 23:57:32 +0300

    forgot to import some objects in views of polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Question
 
